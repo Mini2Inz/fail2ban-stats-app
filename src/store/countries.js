0e3304f2207a1f0ce484d8a7f24511fdb2670b92
Radosław Dąbrowski <radek.dab@gmail.com>
2018-07-09 15:57:09 +0200

    Fetch countries data

---
+++ src/store/countries.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export default {
+  namespaced: true,
+  state: {
+    data: null
+  },
+  getters: {
+    data(state) {
+      if (!state.data) return null;
+      return state.data.default.reduce((map, val, idx) => {
+        map[state.data.labels[idx][0]] = val;
+        return map;
+      }, {});
+    }
+  },
+  mutations: {
+    setData(state, data) {
+      state.data = data;
+    }
+  },
+  actions: {
+    async fetchData({ commit }) {
+      const res = await axios.get('/api/chart/data/pie');
+      commit('setData', res.data);
+    }
+  }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,6 +16,15 @@
   </v-container>
 </template>
 
+<script>
+export default {
+  name: 'Home',
+  created() {
+    this.$store.dispatch('countries/fetchData');
+  }
+};
+</script>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
